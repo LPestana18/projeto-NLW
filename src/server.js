9abb10e2ae785ae67589848fad42dd0b9cdb26e8
LPestana18 <lucaspestana0212@gmail.com>
2020-08-08 11:09:23 -0300

    creating database

--- src/server.js
+++ src/server.js
@@ -3,17 +3,17 @@ const proffys = [
     {
     name: "Diego Fernandes", avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4"
     , whatsapp: "89987654322", bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-    subject: "Química", 
-    cost: "40",
+    subject: "Química",
+    cost: "30", 
      weekday: [0], 
      time_from: [720], 
-     time_to: [1220]
+     time_to: [1220],
     },
     {   
     name: "Daniele Evangelista", avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4"
     , whatsapp: "89987654322", bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química", 
-    cost: "40",
+    cost: "20",
      weekday: [1], 
      time_from: [720], 
      time_to: [1220]
@@ -22,7 +22,7 @@ const proffys = [
     name: "Mayk Brito", avatar: "  https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4"
     , whatsapp: "89987654322", bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química", 
-    cost: "40",
+    cost: "35",
     weekday: [1], 
     time_from: [720], 
     time_to: [1220]
@@ -75,8 +75,9 @@ function pageGiveClasses(req, res){
 
     // se tiver dados
     const isNotEmpty  = Object.keys(data).length > 0   
-    if(data){
+    if(isNotEmpty){
         
+         data.subject = getSubject(data.subject)
          //adicionar dados ao a lista de proffys
          proffys.push(data)
          return res.redirect("/study")
